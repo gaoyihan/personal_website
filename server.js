4dc8941137ead0505c15c5819bc8a2e3118a3510
gaoyihan <ygao34@siebl-1119-01.ad.uillinois.edu>
2017-01-31 12:53:23 -0600

    fix

--- server.js
+++ server.js
@@ -20,8 +20,8 @@ http.createServer( function (request, response) {
          
          // Write the content of the file to response body
          response.write(catamaran.demo());		
-    } else {
-        fs.readFile(pathname.substr(1), function (err, data) {
+    } else if (pathname == '/demo.html') {
+        fs.readFile('catamaran/demo.html', function (err, data) {
             if (err) {
                 console.log(err);
                 // HTTP Status: 404 : NOT FOUND
