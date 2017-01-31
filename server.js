0b5dd42fe331f28543cc6ab0e48d7128226c1ef7
gaoyihan <ygao34@siebl-1119-01.ad.uillinois.edu>
2017-01-31 12:45:42 -0600

    updated server.js

--- server.js
+++ server.js
@@ -13,17 +13,30 @@ http.createServer( function (request, response) {
    
     // Read the requested file content from file system
     if (pathname == '/Demo') {
-         //Page found	  
+         // Page found	  
          // HTTP Status: 200 : OK
          // Content Type: text/plain
-         response.writeHead(200, {'Content-Type': 'text/html'});	
+         response.writeHead(200, {'Content-Type': 'text/html'});
          
          // Write the content of the file to response body
          response.write(catamaran.demo());		
     } else {
-        // HTTP Status: 404 : NOT FOUND
-        // Content Type: text/plain
-        response.writeHead(404, {'Content-Type': 'text/html'});
+        fs.readFile(pathname.substr(1), function (err, data) {
+            if (err) {
+                console.log(err);
+                // HTTP Status: 404 : NOT FOUND
+                // Content Type: text/plain
+                response.writeHead(404, {'Content-Type': 'text/html'});
+            } else {	
+                //Page found	  
+                // HTTP Status: 200 : OK
+                // Content Type: text/plain
+                response.writeHead(200, {'Content-Type': 'text/html'});	
+         
+                // Write the content of the file to response body
+                response.write(data.toString());		
+            }   
+        }
     }
     // Send the response body 
     response.end();
