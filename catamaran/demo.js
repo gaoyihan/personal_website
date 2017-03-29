f21fec3a5fa15df8e336cb3c042e1cf62ed63003
gaoyihan <ygao34@siebl-1119-01.ad.uillinois.edu>
2017-03-29 15:51:17 -0500

    update

--- catamaran/demo.js
+++ catamaran/demo.js
@@ -10,8 +10,26 @@ demo.submit = function() {
     };
 
     var req = new XMLHttpRequest();
-    req.open('GET', '/catamaran/Demo');
+    req.open('GET', '/catamaran/extract-from-string');
     req.onreadystatechange = ajaxResponseHandler;
     req.send();    
     return 
-}
+};
+
+demo.get_table = function() {   
+    var ajaxResponseHandler = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            var response = this.responseText;
+            var text = document.createTextNode(response);
+            document.getElementById('display').appendChild(text);
+        }
+    };
+
+    var req = new XMLHttpRequest();
+    req.open('GET', '/catamaran/get-table');
+    req.onreadystatechange = ajaxResponseHandler;
+    req.send();    
+    return 
+};
+
+demo.get_table();
--- server.js
+++ server.js
@@ -1,50 +1,93 @@
-var http = require("http");
+var express = require('express');
+var app = express();
+var async = require('async');
+var session = require('express-session');
+var FileStore = require('session-file-store');
+
+var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var catamaran = require('./catamaran/catamaran.node');
+var EventEmitter = require('events');
 
-// Create a server
-http.createServer( function (request, response) {  
-    // Parse the request containing file name
-    var pathname = url.parse(request.url).pathname;
-    
-    // Print the name of the file for which request is made.
-    console.log("Request for " + pathname + " received.");
-   
-    // Read the requested file content from file system
-    if (pathname == '/Demo') {
-        // Page found	  
-        // HTTP Status: 200 : OK
-        // Content Type: text/plain
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        
-        // Write the content of the file to response body
-        response.write(catamaran.demo());		
+app.use(express.static('catamaran'));
 
-        // Send the response body 
-        response.end();            
-    } else {
-        fs.readFile('catamaran' + pathname, function (err, data) {
-            if (err) {
-                console.log(err);
-                // HTTP Status: 404 : NOT FOUND
-                // Content Type: text/plain
-                response.writeHead(404, {'Content-Type': 'text/html'});
-            } else {	
-                //Page found	  
-                // HTTP Status: 200 : OK
-                // Content Type: text/plain
-                response.writeHead(200, {'Content-Type': 'text/html'});	
-         
-                // Write the content of the file to response body
-                response.write(data.toString());		
-            }   
-            // Send the response body 
-            response.end();            
-        });
-    }
+app.use(session({
+	name: 'session-id',
+	secret: 'secret',
+	saveUninitialized: true,
+	resave: true,
+	store: new FileStore()
+}));
 
-}).listen(8081);
+catamaran_js.event_listener = new EventEmitter();
 
-// Console will print the message
-console.log('Server running at http://ygao34.web.engr.illinois.edu:8081');
+catamaran_js.extract_from_file = function (filename, schema, callback) {
+	var table = catamaran.extract_from_file(filename, schema);
+	alert(table);
+	callback(table);
+};
+
+catamaran_js.select_schema = function (filename, schema_list, callback) {
+	var schema = catamaran.select_schema(filename, schema_list);
+	alert(schema);
+	callback(schema);
+};
+
+catamaran_js.candidate_gen = function (filename, callback) {
+	var schema_list = catamaran.candidate_gen(filename);
+	alert(schema_list);
+	callback(schema_list);
+};
+
+catamaran_js.extract_struct_from_file = function (filename, callback) {
+	process.nextTick(function() {
+		catamaran_js.candidate_gen(filename, function (schema_list) {
+			process.nextTick(function() {
+				catamaran_js.select_schema(filename, schema_list, function (schema) {
+					process.nextTick(function() {
+						catamaran_js.extract_from_file(filename, schema, callback);
+					});
+				});
+			});
+		});
+	});
+};
+
+catamaran_js.extract_struct_from_string = function (doc, callback) {
+};
+
+app.post('/extract-from-string', function (req, res) {
+	catamaran_js.extract_struct_from_string(req.string, function(table) {
+		res.end( JSON.stringify(table) );
+	});
+});
+
+app.post('/extract-from-file', function (req, res) {
+	var filepath = req.files.uploaded.path;
+	catamaran_js.on(filepath, function(callback) {
+		catamaran_js.extract_struct_from_file(filepath, callback);
+	});
+	req.session.last_access = Date.now();
+	req.session.file_id = filepath;
+	process.setTimeout( function() {
+		if (req.sessions.last_access + 3000000 < Date.now()) {
+			fs.unlinkSync(filepath);
+		}
+	}, 3600000);
+});
+
+app.get('/get-table', function (req, res) {
+	req.session.last_access = Data.now();
+	if (typeof req.session.file_id != 'undefined') {
+		catamaran_js.event_listener.emit(req.session.file_id, function(table) {
+			res.end( JSON.stringify(table) );
+		});
+	} else {
+		res.end('No Table Found');
+	}
+});
+
+var server = app.listen(8081, function() {
+	console.log('Server running at http://ygao34.web.engr.illinois.edu:8081');
+});
