7abc5f96c0fcf78be28c36ec6d34822f46e6f88b
gaoyihan <ygao34@siebl-1119-01.ad.uillinois.edu>
2017-04-01 15:06:31 -0500

    update

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ app.use(express.static('catamaran'));
 var upload = multer({ dest: './tmp/' });
 
 app.use(session({
-	name: 'session-id',
-	secret: 'secret',
-	saveUninitialized: true,
-	resave: true,
-	store: new FileStore()
+    name: 'session-id',
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true,
+    store: new FileStore()
 }));
 
 catamaran_js = {}
@@ -28,70 +28,70 @@ catamaran_js = {}
 catamaran_js.event_listener = new events.EventEmitter();
 
 catamaran_js.extract_from_file = function (filename, schema, callback) {
-	var table = catamaran.extract_from_file(filename, schema);
-	console.log(table);
-	callback(table);
+    var table = catamaran.extract_from_file(filename, schema);
+    console.log('extraction:', table);
+    callback(table);
 };
 
 catamaran_js.select_schema = function (filename, schema_list, callback) {
-	var schema = catamaran.select_schema(filename, schema_list);
-	console.log(schema);
-	callback(schema);
+    var val = catamaran.select_schema(filename, schema_list);
+    console.log('select_schema:', val);
+    callback(val.schema);
 };
 
 catamaran_js.candidate_gen = function (filename, callback) {
-	var schema_list = catamaran.candidate_gen(filename);
-	console.log(schema_list);
-	callback(schema_list);
+    var val = catamaran.candidate_gen(filename);
+    console.log('candidate_gen:', val);
+    callback(val.schema_list);
 };
 
 catamaran_js.extract_struct_from_file = function (filename, callback) {
-	process.nextTick(function() {
-		catamaran_js.candidate_gen(filename, function (schema_list) {
-			process.nextTick(function() {
-				catamaran_js.select_schema(filename, schema_list, function (schema) {
-					process.nextTick(function() {
-						catamaran_js.extract_from_file(filename, schema, callback);
-					});
-				});
-			});
-		});
-	});
+    process.nextTick(function() {
+        catamaran_js.candidate_gen(filename, function (schema_list) {
+            process.nextTick(function() {
+                catamaran_js.select_schema(filename, schema_list, function (schema) {
+                    process.nextTick(function() {
+                        catamaran_js.extract_from_file(filename, schema, callback);
+                    });
+                });
+            });
+        });
+    });
 };
 
 catamaran_js.extract_struct_from_string = function (doc, callback) {
 };
 
 app.post('/extract-from-string', function (req, res) {
-	catamaran_js.extract_struct_from_string(req.string, function(table) {
-		res.end( JSON.stringify(table) );
-	});
+    catamaran_js.extract_struct_from_string(req.string, function(table) {
+        res.end( JSON.stringify(table) );
+    });
 });
 
 app.post('/extract-from-file', upload.single('uploaded'), function (req, res) {
-	var filepath = req.file.path;
-	var listener = function(callback) {
-		catamaran_js.extract_struct_from_file(filepath, callback);
-	};
-	catamaran_js.event_listener.on(filepath, listener);
-	req.session.file_id = filepath;
-	setTimeout( function() {
-		fs.unlinkSync(filepath);
-		catamaran_js.event_listener.removeListener(filepath, listener);
-	}, 3600000);
-	res.redirect('/demo.html')
+    var filepath = req.file.path;
+    var listener = function(callback) {
+        catamaran_js.extract_struct_from_file(filepath, callback);
+    };
+    catamaran_js.event_listener.on(filepath, listener);
+    req.session.file_id = filepath;
+    setTimeout( function() {
+        fs.unlinkSync(filepath);
+        catamaran_js.event_listener.removeListener(filepath, listener);
+    }, 3600000);
+    res.redirect('/demo.html')
 });
 
 app.get('/get-table', function (req, res) {
-	if (typeof req.session.file_id != 'undefined') {
-		catamaran_js.event_listener.emit(req.session.file_id, function(table) {
-			res.end( JSON.stringify(table) );
-		});
-	} else {
-		res.end('No Table Found');
-	}
+    if (typeof req.session.file_id != 'undefined') {
+        catamaran_js.event_listener.emit(req.session.file_id, function(table) {
+            res.end( JSON.stringify(table) );
+        });
+    } else {
+        res.end('No Table Found');
+    }
 });
 
 var server = app.listen(8081, function() {
-	console.log('Server running at http://ygao34.web.engr.illinois.edu:8081');
+    console.log('Server running at http://ygao34.web.engr.illinois.edu:8081');
 });
