58eca3ecb95575fc18e1cc00ce3f337d3d0a4b4d
gaoyihan <ygao34@siebl-1119-01.ad.uillinois.edu>
2017-04-01 15:32:08 -0500

    update

--- catamaran/demo.js
+++ catamaran/demo.js
@@ -4,8 +4,16 @@ demo.submit = function() {
     var ajaxResponseHandler = function() {
         if (this.readyState === 4 && this.status === 200) {
             var response = this.responseText;
-            var text = document.createTextNode(response);
-            document.getElementById('display').appendChild(text);
+            var table_content = JSON.parse(response);
+            var table = document.getElementById('table');
+            for (int i = 0; i < table_content.length; ++i) {
+                var row = table.insertRow(i);
+                for (int j = 0; j < table_content[i].length; ++j) {
+                    var cell = row.insertCell(j);
+                    var text = document.createTextNode(table_content[i][j]);
+                    cell.appendChild(text);
+                }
+            }
         }
     };
 
