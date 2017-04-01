814e4187f9bf8e5fdb850b885d0c9fd18347b351
gaoyihan <ygao34@siebl-1119-01.ad.uillinois.edu>
2017-04-01 15:41:26 -0500

    fix

--- catamaran/demo.js
+++ catamaran/demo.js
@@ -6,9 +6,9 @@ demo.submit = function() {
             var response = this.responseText;
             var table_content = JSON.parse(response);
             var table = document.getElementById('table');
-            for (int i = 0; i < table_content.length; ++i) {
+            for (i = 0; i < table_content.length; ++i) {
                 var row = table.insertRow(i);
-                for (int j = 0; j < table_content[i].length; ++j) {
+                for (j = 0; j < table_content[i].length; ++j) {
                     var cell = row.insertCell(j);
                     var text = document.createTextNode(table_content[i][j]);
                     cell.appendChild(text);
